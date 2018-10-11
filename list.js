7067cd834571576c216e8ad2e820e9c43cd5cce9
hville <hugov@runbox.com>
2018-10-11 16:40:15 -0400

    0.11.2

--- list.js
+++ list.js
@@ -35,25 +35,28 @@ function updateList(arr) {
 			getK = this.key,
 			kids = Object.create(null)
 
-	for (var i = 0; i < arr.length; ++i) {
-		var key = getK.constructor === Function ? getK(arr[i], i, arr) : arr[i][getK],
-				kid = this.map[key]
-		//create or update kid
-		if (kid) kid.update && kid.update(arr[i], key, arr) //eslint-disable-line
-		else kid = this.factory(arr[i], i, arr)
-		kids[key] = kid
+	if (!arr.length && !this.before && !this.after) parent.textContent = ''
+	else {
+		for (var i = 0; i < arr.length; ++i) {
+			var key = getK.constructor === Function ? getK(arr[i], i, arr) : arr[i][getK],
+					kid = this.map[key]
+			//create or update kid
+			if (kid) kid.update && kid.update(arr[i], key, arr) //eslint-disable-line
+			else kid = this.factory(arr[i], i, arr)
+			kids[key] = kid
 
-		//place kid
-		if (!spot) parent.appendChild(kid)
-		else if (kid === spot.nextSibling) parent.removeChild(spot)
-		else if (kid !== spot) parent.insertBefore(kid, spot)
-		spot = kid.nextSibling
-	}
-	//delete remaining
-	while (spot !== this.before) {
-		var next = spot.nextSibling
-		parent.removeChild(spot)
-		spot = next
+			//place kid
+			if (!spot) parent.appendChild(kid)
+			else if (kid === spot.nextSibling) parent.removeChild(spot)
+			else if (kid !== spot) parent.insertBefore(kid, spot)
+			spot = kid.nextSibling
+		}
+		//delete remaining
+		while (spot !== this.before) {
+			var next = spot.nextSibling
+			parent.removeChild(spot)
+			spot = next
+		}
 	}
 
 	this.map = kids
